93fc49490b4fd4ccbfb178e75cf9b4cca98bad80
Gabriel Frasson <bielfc27@gmail.com>
2023-04-27 00:16:38 -0300

    Improvements on controls and accessibility visualization

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -3,7 +3,13 @@ module.exports = {
     "../src/pages/**/*.stories.mdx",
     "../src/stories/**/*.stories.tsx"
   ],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+    "@storybook/addon-a11y"
+  ],
   "framework": {
     name: "@storybook/react-vite",
     options: {}
--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/GFrasson.png',
     alt: 'Gabriel Frasson',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
